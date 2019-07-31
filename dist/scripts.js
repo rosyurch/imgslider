c1a8868b20050944729ce5d3e513698ce7ef7243
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-07-31 22:25:07 +0300

    two buttons, some styles

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,6 +1,6 @@
-const images = document.querySelectorAll('.slider .pics > img');
-const prevBtn = document.querySelector('.slider .btn-prev');
-const nextBtn = document.querySelector('.slider .btn-next');
+const images = document.querySelectorAll('.slider .container .pics > img');
+const prevBtn = document.querySelector('.slider .container .btn-prev');
+const nextBtn = document.querySelector('.slider .container .btn-next');
 
 imagesArr = Array.from(images);
 
@@ -9,4 +9,13 @@ nextBtn.addEventListener('click', event => {
     curIndex = imagesArr.indexOf(curr[0]);
     curr[0].classList.remove('current'); // filter returns 1-element array
     imagesArr[(curIndex + 1) % imagesArr.length].classList.add('current');
+});
+
+prevBtn.addEventListener('click', event => {
+    let curr = imagesArr.filter(image => image.classList.contains('current'));
+    curIndex = imagesArr.indexOf(curr[0]);
+    curr[0].classList.remove('current');
+    imagesArr[curIndex ? curIndex - 1 : imagesArr.length - 1].classList.add(
+        'current'
+    );
 });
