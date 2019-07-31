11de885aeee7a5c6c29cd1426c5d4b2c55da97dd
Ros Yurchyk <ros_ur@ukr.net>
2019-07-31 17:45:44 +0300

    next button works

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log('FFFFFFFFFFFFFF');
+const images = document.querySelectorAll('.slider .pics > img');
+const prevBtn = document.querySelector('.slider .btn-prev');
+const nextBtn = document.querySelector('.slider .btn-next');
+
+imagesArr = Array.from(images);
+
+nextBtn.addEventListener('click', event => {
+    let curr = imagesArr.filter(image => image.classList.contains('current'));
+    curIndex = imagesArr.indexOf(curr[0]);
+    curr[0].classList.remove('current'); // filter returns 1-element array
+    imagesArr[(curIndex + 1) % imagesArr.length].classList.add('current');
+});
